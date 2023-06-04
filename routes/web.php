da11f155699d6d39afc3be7bf3f46561db799e8c
maulana677 <maull0776@gmail.com>
2023-06-04 09:20:35 +0700

    konversi home page

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
