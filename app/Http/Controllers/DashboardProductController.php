3d442e922974de23536b517e0baa3d9a666db584
maulana677 <maull0776@gmail.com>
2023-07-08 14:43:49 +0700

    Integrasi Dashboard My Product add action

--- app/Http/Controllers/DashboardProductController.php
+++ app/Http/Controllers/DashboardProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Admin\ProductRequest;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductGallery;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,5 +32,22 @@ class DashboardProductController extends Controller
         return view('pages.dashboard-products-create',[
             'categories' => $categories
         ]);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+        $product = Product::create($data);
+
+        $gallery = [
+            'products_id' => $product->id,
+            'photo' => $request->file('photo')->store('assets/product','public')
+        ];
+
+        ProductGallery::create($gallery);
+
+        return redirect()->route('dashboard-product');
     }
 }
